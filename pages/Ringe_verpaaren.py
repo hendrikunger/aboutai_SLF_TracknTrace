86d7c538d20c3980ee1fd82f9cdb31f05db0301e
Hendrik Unger <unger.hendrik@gmail.com>
2024-08-31 22:08:09 +0200

    simple camera flow

--- pages/Ringe_verpaaren.py
+++ pages/Ringe_verpaaren.py
@@ -7,7 +7,8 @@ import asyncio
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import NoResultFound
-
+import serial_asyncio
+import serial
 
 main_project_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
@@ -27,10 +28,9 @@ pn.extension('tabulator', notifications=True)
 pn.state.notifications.position = 'top-right'
 engine = create_engine(f"postgresql+psycopg2://{config['DATABASE']}", echo=True)
 
-if "currentSerialIDs" not in pn.state.cache:
-    pn.state.cache["currentSerialIDs"] =  pn.rx([""])
+currentSerialIDs =  pn.rx([""])
 
-currentSerialIDs = pn.state.cache["currentSerialIDs"]
+
 
 #read links from json file
 with open("links.json", "r") as f:
@@ -66,10 +66,7 @@ def update_currentSerialIDs(event):
     currentSerialIDs.rx.value = [event.new]
 
 def onCurrentSerialIDsChange(newIDs):
-    print(f"New IDs: {newIDs}", flush=True)
-    #if len(newIDs) == 0: return
     for dmc in newIDs:
-        print(f"DMC: {dmc}", flush=True)
         if not dmc.isdigit():
             pn.state.notifications.error(f'DMC {dmc} ist keine Zahl', duration=2000)
             print(f"DMC {dmc} is not a number", flush=True)
@@ -91,40 +88,43 @@ async def button_save_function(event):
 
 
 # Define an asynchronous function to handle TCP/IP communication
-async def tcp_ip_client():
-    currentSerialIDs = pn.state.cache["currentSerialIDs"]
-    while True:
-        #callback =  pn.state.add_periodic_callback(lambda: setattr(currentSerialIDs.rx, 'value', data), period=200)
+async def tcp_ip_client(event):
+    reader, writer = await serial_asyncio.open_serial_connection(url="COM3", 
+                                                                    baudrate=115200,
+                                                                    bytesize=serial.EIGHTBITS,
+                                                                    parity=serial.PARITY_EVEN,
+                                                                    stopbits=serial.STOPBITS_ONE,
+                                                                    )
 
-        reader, writer = await asyncio.open_connection('localhost', 3000)
-        while True:
-                line = await reader.readline()
-                if not line:
-                    break
-                data = line.decode('utf8').rstrip()
-                print(f'Received: {data}', flush=True)
-                #data = data.replace(",", "\n")
-                data = data.split(",")
-                data = [d.split(":")[1] for d in data]
-                data = [d for d in data if d.isdigit()]
-                callback =  pn.state.add_periodic_callback(lambda: setattr(currentSerialIDs.rx, 'value', data), period=200, count=1)
+    message = f'LON\r'
+    print(f'Sending:{message}', flush=True)
+    writer.write(message.encode())
+    await writer.drain()
+    line = await reader.readuntil(separator=b'\r')
+    if not line:
+        return
+    data = line.decode('utf8').rstrip()
+    print(f'Received: {data}', flush=True)
+    if "Error"  in data:
+        pn.state.notifications.error(f'DMC Lesefehler: {data}', duration=0)
+        return
+    data = data.split(",")
+    if len(data) < 3:
+        pn.state.notifications.error(f'DMC Lesefehler weniger als 5 DMC gefunden', duration=0)
+        return
+    data = [d for d in data if d.isdigit()]
+    currentSerialIDs.rx.value = data
             
-                await asyncio.sleep(0.1)
+    writer.close()
+    await writer.wait_closed()
+    return
 
-        if writer:
-            writer.close()
-            await writer.wait_closed()
-        if callback:
-            await callback.stop()
-        # Wait before attempting to reconnect
-        print("Reconnecting in 10 seconds...", flush=True)
-        await asyncio.sleep(10)
-
-# Function to start the asyncio event loop
-async def run_asyncio():
-    await tcp_ip_client()
-
-
+b_Scan = pn.widgets.Button(name='DMC Scannen',
+                           button_type='primary',
+                           height=80,
+                           sizing_mode="stretch_width",
+                           disabled=False,)
+b_Scan.rx.watch(tcp_ip_client)
     
 
 b_Save = pn.widgets.Button(name='Auswahl speichern',
@@ -164,10 +164,6 @@ ti_Barcode.param.watch(update_currentSerialIDs, "value")
 currentSerialIDs.rx.watch(onCurrentSerialIDsChange, "value")
 
 
-# md_currentSerialIDs = pn.pane.Markdown(currentSerialIDs,
-#                                       width=250,
-#                                       styles={'text-align': 'center',
-#                                       'font-size': '24px'})
 
 df = pd.DataFrame({"SerialID": ["34534534","33345"]})
 tabulator_options = {
@@ -183,14 +179,6 @@ custom_css = """
 pn.config.raw_css.append(custom_css)
 tabulator_widget = pn.widgets.Tabulator(df, width=400, show_index=False, theme="site", widths={"SerialID": 400},configuration=tabulator_options)
 
-# serialCardID = pn.Card(pn.Row(pn.Spacer(sizing_mode="stretch_width"),
-#                                         md_currentSerialIDs,
-#                                         pn.Spacer(sizing_mode="stretch_width")),
-#                                         width=250,
-#                                         height=80,
-#                                         hide_header=True)
-
-
 
    
 
@@ -203,14 +191,14 @@ running_indicator = pn.indicators.LoadingSpinner(value=False,
 
 
 
-column = pn.Column(pn.Row(pn.Spacer(sizing_mode="stretch_width"),pn.pane.Markdown("# Aktuelle Seriennummer:"), pn.Spacer(sizing_mode="stretch_width")),
+column = pn.Column(pn.Row(pn.Spacer(sizing_mode="stretch_width"),pn.pane.Markdown("# Aktuelle Seriennummer(n):"), pn.Spacer(sizing_mode="stretch_width")),
                    pn.Row(pn.Spacer(sizing_mode="stretch_width"), tabulator_widget, pn.Spacer(sizing_mode="stretch_width")),
+                   b_Scan,
                    pn.Row(pn.Spacer(sizing_mode="stretch_width"),pn.pane.Markdown("# Außenring Gruppe:"), pn.Spacer(sizing_mode="stretch_width")),
                    pn.Row(pn.Spacer(sizing_mode="stretch_width"),ar_group, pn.Spacer(sizing_mode="stretch_width")),
                    pn.Row(pn.Spacer(sizing_mode="stretch_width"),pn.pane.Markdown("# Innenring Gruppe:"), pn.Spacer(sizing_mode="stretch_width")),
                    pn.Row(pn.Spacer(sizing_mode="stretch_width"),ir_group, pn.Spacer(sizing_mode="stretch_width")),
-                   pn.Spacer(sizing_mode="stretch_width", height=100),
-                   b_Save,
+                   pn.Spacer(sizing_mode="stretch_width", height=100), b_Save,
                    pn.Row(pn.Spacer(sizing_mode="stretch_width"), running_indicator, pn.Spacer(sizing_mode="stretch_width")),
                    pn.Spacer(sizing_mode="stretch_width", height=100),
                    pn.Row(pn.Spacer(sizing_mode="stretch_width"), ti_Barcode, pn.Spacer(sizing_mode="stretch_width")),
@@ -228,9 +216,4 @@ pn.template.BootstrapTemplate(
 ).servable()
 
 
-#Ensure the asyncio event loop runs in the background
-if not hasattr(pn.state, 'asyncio_task'):
-    loop = asyncio.get_event_loop()
-    pn.state.asyncio_task = loop.create_task(run_asyncio())
-    print("Asyncio task started.", flush=True)
 
