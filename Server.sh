fccc8267371a7666faa07de59bf515934346aee0
Hendrik Unger <unger.hendrik@gmail.com>
2024-09-03 15:50:58 +0200

    test

--- Server.sh
+++ Server.sh
@@ -1,3 +1,4 @@
+eval "$(micromamba shell hook --shell bash)"
 micromamba activate slf
 cd "C:/Users/admin/AppData/Local/aboutai_SLF_TracknTrace"
 C:/Users/admin/micromamba/envs/slf/Scripts/panel.exe serve "pages\AR_DMC_Lasern.py" --autoreload --port 80 --admin  --static-dirs assets=./assets --log-level warning
